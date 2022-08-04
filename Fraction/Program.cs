d1c766366683a1079b98e0997d67c9eb2c03cf5b
Leyla Shafiyeva <llshafi03@gmail.com>
2022-08-04 22:41:18 +0400

    Add project files.

---
+++ Fraction/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace FractionProg;
+
+class Fraction
+{
+    private int _numerator { get; set; }
+    private int _denominator { get; set; }
+
+    public Fraction()
+    {
+        _numerator = 0;
+        _denominator = 0;
+    }
+
+    public Fraction(int num, int denom)
+    {
+        _numerator=num;
+        _denominator=denom;
+    }
+
+    public Fraction Multiply(in Fraction other)
+    {
+        Fraction f= new Fraction(_numerator* other._numerator, _denominator* other._denominator);
+        return f;
+       
+    }
+    public Fraction Divide(in Fraction other)
+    {
+        if (other._denominator != 0)
+        {
+            Fraction f = new Fraction(_numerator + other._numerator, _denominator + other._denominator);
+            return f;
+        }
+        return this;
+        
+    }
+    public Fraction Add(in Fraction other)
+    {
+        Fraction f = new Fraction(_numerator + other._numerator, _denominator + other._denominator);
+        return f;
+    }
+    public Fraction Subtract(in Fraction other)
+    {
+
+         Fraction f= new Fraction(_numerator - other._numerator, _denominator - other._denominator);
+         return f;
+    }
+    public override string ToString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+}
+
+
+class FragtionProgram
+{
+    static void Main()
+    {
+        Fraction fraction1 = new Fraction(10,5);
+        Fraction fraction2 = new Fraction(5,0);
+        //Console.WriteLine(fraction1.Add(fraction2).ToString());
+        //Console.WriteLine(fraction1.Subtract(fraction2).ToString());
+        //Console.WriteLine(fraction1.Multiply(fraction2).ToString());
+        //Console.WriteLine(fraction1.Divide(fraction2).ToString());
+    }
+}
+
---
+++ Homework4Csharp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Program;
+
+struct Point
+{
+    private int X { get; set; }
+    private int Y { get; set; }
+
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+    }
+
+    public Point(int _x, int _y)
+    {
+        X = _x;
+        Y = _y;
+    }
+
+}
+
+class Counter
+{
+    private int Min;
+    private int Max;
+    private int Cur;
+
+
+    public Counter(int min, int max)
+    {
+        Min = min;
+        Max = max;
+        Cur = min;
+    }
+
+    public void Increment()
+    {
+        if (Cur < Max)
+            Cur++;
+        else Cur = Min;
+
+    }
+    public void Decrement()
+    {
+        if (Cur > Min)
+            Cur--;
+        else Cur = Max;
+    }
+
+    public int getCurrent()
+    {
+        return Cur;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Counter counter = new Counter(1, 5);
+        //counter.Increment();
+        counter.Decrement();
+        //counter.Decrement();
+        Console.WriteLine(counter.getCurrent());
+
+    }
+}
